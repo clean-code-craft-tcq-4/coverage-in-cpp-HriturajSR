9b0c26aabbd8370ef2a53d0535ded88412bcd5e0
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-07-22 13:10:06 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,13 +3,6 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-PASSIVE_COOLING passive_cooling;
-HI_ACTIVE_COOLING hiactive_cooling;
-MED_ACTIVE_COOLING medactive_cooling;
-BatteryCharacter<PASSIVE_COOLING> batteryChar_passive;
-BatteryCharacter<HI_ACTIVE_COOLING> batteryChar_hiactive;
-BatteryCharacter<MED_ACTIVE_COOLING> batteryChar_medactive;
-
 TEST_CASE("infers the breach according to limits") {
 REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
@@ -33,15 +26,9 @@ REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 50) == TOO_HIGH);
 
 TEST_CASE( "Check and alert status to controller") {
 
-REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar_passive, 12) == SENT );
-REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar_hiactive, 26) == SENT );
-REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar_medactive, 8) == SENT );
+  BatteryCharacter batteryChar {PASSIVE_COOLING,"LG_Li-po"};
+	checkAndAlert(TO_CONTROLLER, batteryChar, LOW_VALUE);
+	checkAndAlert(TO_CONTROLLER, batteryChar, MID_VALUE);
+	checkAndAlert(TO_CONTROLLER, batteryChar, HIGH_VALUE);
 
-}
-
-TEST_CASE( "Check and alert status to email") {
-
-REQUIRE(checkAndAlert(TO_EMAIL, batteryChar_passive, 12) == SENT );
-REQUIRE(checkAndAlert(TO_EMAIL, batteryChar_hiactive, 46) == SENT );
-REQUIRE(checkAndAlert(TO_EMAIL, batteryChar_medactive, 0) == SENT );
 }
